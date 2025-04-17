7713dec4db9383b415b94bd6be1b4ee55fe3ba8d
ravencoded <mert2684ural@gmail.com>
2025-04-18 02:01:05 +0300

    added: lecture 1 is done. I learnd print function in C language

---
+++ Lecture1/welcomeToC.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("----------------- \n");
+    printf("MERT\tURAL\n");
+    printf("------------------ \n " ) ;
+    printf("ravencoded\n\a" ) ;
+    printf("------------------ \n " ) ;
+    printf("printf\tpratik\n" ) ;
+    printf("------------------------ \n" ) ;
+    printf("|                       |\n" ) ;
+    printf("|                       |\n" ) ;
+    printf("|                       |\n" ) ;
+    printf("|   ravencodedtraining  |\n" ) ;
+    printf("|                       |\n" ) ;
+    printf("|                       |\n" ) ;
+    printf("|                       |\n" ) ;
+    printf("-------------------------\n ") ;
+
+
+    return 0;
+}
