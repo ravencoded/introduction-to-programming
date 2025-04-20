28bb10a9b0423c43ccd58e730b574a61443a1d57
ravencoded <mert2684ural@gmail.com>
2025-04-21 00:48:17 +0300

    added: scanf function, i learned how to get a value from user with using scanf

---
+++ Lecture2/scanfPractice.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+ int main() {
+   int x;
+     int y;
+     printf("bir sayi girin:\n");
+     scanf("%d",&x);
+     printf("\ngirdiginiz ilk sayi:%d\n",x);
+     printf("bir sayi girin:\n");
+     scanf("%d",&y);
+     printf("\ngirdiginiz ilk sayi:%d\n",y);
+
+     return 0;
+ }
---
+++ Lecture2/scanfPractice2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main() {
+ int x ;
+ int y ;
+ int z;
+
+ printf("Enter a number : ");
+ scanf("%d", &x);
+
+ printf("\ngirdiginiz ilk sayi:%d\n",x);
+
+ printf("enter a number : ");
+ scanf("%d", &y);
+
+ printf("\ngirdiginiz ikinci sayi:%d\n",y);
+
+ z = x + y;
+ printf("toplam: %d\n",z);
+
+ z = x - y;
+ printf("cikarma: %d\n",z);
+
+ z = x * y;
+ printf("carpma: %d\n",z);
+
+ z = x / y;
+ printf("bolme: %d\n",z);
+
+ return 0;
+}
