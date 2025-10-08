291918810a47f9caf1d63e22964ca913650806fe
ravencoded <mert2684ural@gmail.com>
2025-10-09 00:14:26 +0300

    I solved the problemfrom yesterday

--- Lecture6/whiledoWhileFor.4c.c
+++ Lecture6/whiledoWhileFor.4c.c
@@ -3,21 +3,21 @@ int main() {
     int aklimdakisayi = 50 ;
     int tahmin;
     printf("sayi tahmin oyununa hosgeldiniz");
-    printf("1 ile 100 arasında bir sayı tuttum aklımdanbulmaya çalış");
+    printf("1 ile 100 arasinda bir sayi tuttum aklimdan bulmaya calıs");
     do {
         printf("sayiyi tahmin edin:\n ");
         scanf("%d",&tahmin);
         if (tahmin > aklimdakisayi) {
-            printf("daha düsük bir sayi giriniz");
+            printf("daha dusuk bir sayi giriniz");
         }
         else if (tahmin < aklimdakisayi) {
-            printf("daha yüksek bir sayi giriniz");
+            printf("daha yuksek bir sayi giriniz");
         }
         else{
-            printf("doğru tahmin ettin");
+            printf("dogru tahmin ettin");
 
         }
-        while (aklimdakisayi==tahmin);
+    }while (aklimdakisayi=!tahmin);
 
 
 
