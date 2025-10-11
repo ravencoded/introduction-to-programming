47e6f25e6f414f5cffdae7b75eca784c5d8dfcf4
ravencoded <mert2684ural@gmail.com>
2025-10-11 23:48:14 +0300

    this is my homework

---
+++ Lecture7/Function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int bes_ekle(int x);
+int karesini_al(int x);
+int main() {
+   int sayi = 10;
+   int sayi2 = 3;
+   printf("%d\n",bes_ekle(sayi));
+   printf("%d\n",karesini_al(sayi2));
+   return 0;
+}
+int bes_ekle(int x) {
+   x+= 5 ;
+   return x ;
+}
+int karesini_al(int x) {
+   x*=x;
+   return x ;
+}
