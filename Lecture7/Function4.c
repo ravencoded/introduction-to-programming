e054d73146e000820b2b6409fdde4ca43df4c043
ravencoded <mert2684ural@gmail.com>
2025-10-14 15:03:08 +0300

    I practiced about functions

---
+++ Lecture7/Function4.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int delta(int a,int b,int c) {
+    return (b*b) - (4*a*c);
+}
+int main() {
+    int a,b,c;
+    printf("katsayilari giriniz\n");
+    scanf("%d,%d,%d",&a,&b,&c);
+    printf("%d\n",delta(a,b,c));
+    return 0;
+}
